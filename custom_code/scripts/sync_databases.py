d677c4343684c4d8c961376f1e15871bd34d386c
Moira Andrews <moira.lee.andrews@gmail.com>
2025-12-08 13:24:23 -0800

    adding another logging statement

--- custom_code/scripts/sync_databases.py
+++ custom_code/scripts/sync_databases.py
@@ -747,6 +747,7 @@ def run():
     """
     actions = ['delete', 'insert', 'update']
     for action in actions:
+        logger.info(f'Updating for action: {action}')
         update_groups(action, db_address=_SNEX2_DB)
         logger.info('Done updating groups')
         update_users(action, db_address=_SNEX2_DB)
