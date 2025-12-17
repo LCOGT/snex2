ba8a74aa4029e7ab1d0c445162364820be628808
Joseph Farah <jrfarah1999@gmail.com>
2025-12-17 04:26:07 +0000

    functionalizing some spectra code per curtis comment

--- custom_code/dash_apps/spectra.py
+++ custom_code/dash_apps/spectra.py
@@ -22,6 +22,7 @@ from tom_dataproducts.models import ReducedDatum
 from custom_code.templatetags.custom_code_tags import bin_spectra
 from django.templatetags.static import static
 import matplotlib.pyplot as plt
+from custom_code.dash_apps.spectra_utils import elements, calculate_flux_range
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
@@ -31,28 +32,6 @@ app.css.append_css({'external_url': static('custom_code/css/dash.css')})
 params = [
     'Redshift', 'Velocity (km/s)'
 ]
-
-elements = {
-    'H': {'color': '#ff0000', 'waves': [3970, 4102, 4341, 4861, 6563]},
-    'He': {'color': '#002157', 'waves': [4472, 5876, 6678, 7065]},
-    'He II': {'color': '#003b99', 'waves': [3203, 4686]},
-    'O': {'color': '#007236', 'waves': [7774, 7775, 8447, 9266]},
-    'O II': {'color': '#00a64d', 'waves': [3727]},
-    'O III': {'color': '#00bf59', 'waves': [4959, 5007]},
-    'Na': {'color': '#aba000', 'waves': [5890, 5896, 8183, 8195]},
-    'Mg': {'color': '#8c6239', 'waves': [2780, 2852, 3829, 3832, 3838, 4571, 5167, 5173, 5184]},
-    'Mg II': {'color': '#bf874e', 'waves': [2791, 2796, 2803, 4481]},
-    'Si II': {'color': '#5674b9', 'waves': [3856, 5041, 5056, 5670, 6347, 6371]},
-    'S II': {'color': '#a38409', 'waves': [5433, 5454, 5606, 5640, 5647, 6715]},
-    'Ca II': {'color': '#005050', 'waves': [3934, 3969, 7292, 7324, 8498, 8542, 8662]},
-    'Fe II': {'color': '#f26c4f', 'waves': [5018, 5169]},
-    'Fe III': {'color': '#f9917b', 'waves': [4397, 4421, 4432, 5129, 5158]},
-    'C II': {'color': '#303030', 'waves': [4267, 4745, 6580, 7234]},
-    'Galaxy': {'color': '#000000', 'waves': [4341, 4861, 6563, 6548, 6583, 6300, 3727, 4959, 5007, 2798, 6717, 6731]},
-    'Tellurics': {'color': '#b7b7b7', 'waves': [6867, 6884, 7594, 7621]},
-    'Flash CNO': {'color': '#0064c8', 'waves': [4648, 5696, 5801, 4640, 4058, 4537, 5047, 7109, 7123, 4604, 4946, 3410, 5597, 3811, 3835]},
-    'SN Ia': {'color': '#ff9500', 'waves': [3856, 5041, 5056, 5670, 6347, 6371, 5433, 5454, 5606, 5640, 5647, 6715, 3934, 3969, 7292, 7324, 8498, 8542, 8662]},
-}
 tooltips = [{
     'value': 'rest wavelengths: ' + str(elements[elem]['waves']),
     'type': 'text',
@@ -391,20 +370,7 @@ def display_output(selected_rows,
             fig_data['data'].remove(d)
     
     # Calculate actual min/max flux from spectrum data (not element lines)
-    actual_min_flux = min_flux
-    actual_max_flux = max_flux
-    for trace in graph_data['data']:
-        if trace['name'] not in elements and 'y' in trace and trace['y']:
-            y_vals = [v for v in trace['y'] if v is not None]
-            if y_vals:
-                trace_min = min(y_vals)
-                trace_max = max(y_vals)
-                if actual_min_flux == 0 and actual_max_flux == 0:
-                    actual_min_flux = trace_min
-                    actual_max_flux = trace_max
-                else:
-                    actual_min_flux = min(actual_min_flux, trace_min)
-                    actual_max_flux = max(actual_max_flux, trace_max)
+    actual_min_flux, actual_max_flux = calculate_flux_range(graph_data, min_flux, max_flux)
     
     for row in selected_rows:
         for elem, row_extras in json.loads(row).items():
--- custom_code/dash_apps/spectra_individual.py
+++ custom_code/dash_apps/spectra_individual.py
@@ -20,6 +20,7 @@ from django.db.models import Q
 from django.templatetags.static import static
 import matplotlib.pyplot as plt
 import logging
+from custom_code.dash_apps.spectra_utils import elements, calculate_flux_range
 
 logger = logging.getLogger(__name__)
 
@@ -31,28 +32,6 @@ app.css.append_css({'external_url': static('custom_code/css/dash.css')})
 params = [
     'Redshift', 'Velocity (km/s)'
 ]
-
-elements = {
-    'H': {'color': '#ff0000', 'waves': [3970, 4102, 4341, 4861, 6563]},
-    'He': {'color': '#002157', 'waves': [4472, 5876, 6678, 7065]},
-    'He II': {'color': '#003b99', 'waves': [3203, 4686]},
-    'O': {'color': '#007236', 'waves': [7774, 7775, 8447, 9266]},
-    'O II': {'color': '#00a64d', 'waves': [3727]},
-    'O III': {'color': '#00bf59', 'waves': [4959, 5007]},
-    'Na': {'color': '#aba000', 'waves': [5890, 5896, 8183, 8195]},
-    'Mg': {'color': '#8c6239', 'waves': [2780, 2852, 3829, 3832, 3838, 4571, 5167, 5173, 5184]},
-    'Mg II': {'color': '#bf874e', 'waves': [2791, 2796, 2803, 4481]},
-    'Si II': {'color': '#5674b9', 'waves': [3856, 5041, 5056, 5670, 6347, 6371]},
-    'S II': {'color': '#a38409', 'waves': [5433, 5454, 5606, 5640, 5647, 6715]},
-    'Ca II': {'color': '#005050', 'waves': [3934, 3969, 7292, 7324, 8498, 8542, 8662]},
-    'Fe II': {'color': '#f26c4f', 'waves': [5018, 5169]},
-    'Fe III': {'color': '#f9917b', 'waves': [4397, 4421, 4432, 5129, 5158]},
-    'C II': {'color': '#303030', 'waves': [4267, 4745, 6580, 7234]},
-    'Galaxy': {'color': '#000000', 'waves': [4341, 4861, 6563, 6548, 6583, 6300, 3727, 4959, 5007, 2798, 6717, 6731]},
-    'Tellurics': {'color': '#b7b7b7', 'waves': [6867, 6884, 7594, 7621]},
-    'Flash CNO': {'color': '#0064c8', 'waves': [4648, 5696, 5801, 4640, 4058, 4537, 5047, 7109, 7123, 4604, 4946, 3410, 5597, 3811, 3835]},
-    'SN Ia': {'color': '#ff9500', 'waves': [3856, 5041, 5056, 5670, 6347, 6371, 5433, 5454, 5606, 5640, 5647, 6715, 3934, 3969, 7292, 7324, 8498, 8542, 8662]},
-}
 tooltips = [{
     'value': 'rest wavelengths: ' + str(elements[elem]['waves']),
     'type': 'text',
@@ -622,20 +601,7 @@ def display_output(selected_rows,
         graph_data['layout']['yaxis']['autorange'] = False
     
     # Calculate actual min/max flux from spectrum data for element lines
-    actual_min_flux = min_flux
-    actual_max_flux = max_flux
-    for trace in graph_data['data']:
-        if trace['name'] not in elements.keys() and 'y' in trace and trace['y']:
-            y_vals = [v for v in trace['y'] if v is not None]
-            if y_vals:
-                trace_min = min(y_vals)
-                trace_max = max(y_vals)
-                if actual_min_flux == 0 and actual_max_flux == 0:
-                    actual_min_flux = trace_min
-                    actual_max_flux = trace_max
-                else:
-                    actual_min_flux = min(actual_min_flux, trace_min)
-                    actual_max_flux = max(actual_max_flux, trace_max)
+    actual_min_flux, actual_max_flux = calculate_flux_range(graph_data, min_flux, max_flux)
     
     for row in selected_rows:
         (elem, row_extras), = json.loads(row).items()
