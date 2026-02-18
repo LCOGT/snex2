763691bb09c37216f51d32cf9121ce9920a64ffa
Moira Andrews <moira.lee.andrews@gmail.com>
2026-02-18 11:50:50 -0800

    added dynamic cadence check to conversion

--- snex2/conversion.py
+++ snex2/conversion.py
@@ -1,4 +1,4 @@
-from tom_observations.models import ObservationRecord
+from tom_observations.models import ObservationRecord, DynamicCadence
 import logging
 
 logger = logging.getLogger(__name__)
@@ -9,6 +9,17 @@ def convert_cadence_strategy():
             obs.parameters['cadence_strategy'] = 'ResumeCadenceAfterFailureStrategy'
             logger.info(f'Converted resume strategy for {obs.target} with obs id: {obs.id}')
 
-        if obs.parameters.get('cadence_strategy') == 'SnexRetryCadenceAfterFailureStrategy':
-            obs.parameters['cadence_strategy'] = 'RetryCadenceAfterFailureStrategy'
+        if obs.parameters.get('cadence_strategy') == 'SnexRetryFailedObservationsStrategy':
+            obs.parameters['cadence_strategy'] = 'RetryFailedObservationsStrategy'
             logger.info(f'Converted single time observation strategy for {obs.target} with obs id: {obs.id}')
+    
+    cads = DynamicCadence.objects.all()
+    for cad in cads:
+        if cad.cadence_strategy == 'SnexResumeCadenceAfterFailureStrategy':
+            cad.cadence_strategy = 'ResumeCadenceAfterFailureStrategy'
+            logger.info(f'Converted cadence strategy for {cad.observation_group_id} with cad id: {cad.id}')
+
+        if cad.cadence_strategy == 'SnexRetryFailedObservationsStrategy':
+            cad.cadence_strategy = 'RetryFailedObservationsStrategy'
+            logger.info(f'Converted single time cadence strategy for {cad.target} with cad id: {cad.id}')
+    
