07e303758810a23d8ff779c883f03815c65d7a95
Moira Andrews <moira.lee.andrews@gmail.com>
2026-03-16 14:34:15 -0700

    add converter for cadence_freq_days and reminder_date

--- custom_code/management/commands/convert_obsrecs.py
+++ custom_code/management/commands/convert_obsrecs.py
@@ -6,7 +6,7 @@ def contains_capital_regex(s):
     return bool(re.search(r'[A-Z]', s))
 
 #only should need to run once to convert the obsrecord start_user to username
-def convert_obs_records():
+def convert_start_user():
     obsrecs = ObservationRecord.objects.all()
     for obs in obsrecs:
         start_user = obs.parameters.get('start_user')
@@ -18,4 +18,22 @@ def convert_obs_records():
                 obs.save()
             else:
                 print(start_user)
-        
+
+def convert_field_to_date():
+    obsrecs = ObservationRecord.objects.all()
+    for obs in obsrecs:
+        cad_freq_days = obs.parameters.get('cadence_frequency_days')
+        if not cad_freq_days:
+            cad_freq = obs.parameters.get('cadence_frequency')
+            if cad_freq:
+                print(cad_freq_days,cad_freq)
+                obs.parameters['cadence_frequency_days'] = cad_freq
+                obs.save()
+        reminder_date = obs.parameters.get('reminder_date')
+        if not reminder_date:
+            reminder = obs.parameters.get('reminder')
+            if reminder:
+                obs.parameters['reminder_date'] = reminder
+                obs.save()
+
+
