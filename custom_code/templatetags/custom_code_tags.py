08a68dfcdfe58d9e1bc4702efb30e7125a50022d
Moira Andrews <moira.lee.andrews@gmail.com>
2026-03-16 13:06:49 -0700

    adding logging

--- custom_code/templatetags/custom_code_tags.py
+++ custom_code/templatetags/custom_code_tags.py
@@ -963,6 +963,7 @@ def format_lco_summary(obs, group, is_active):
             summary.append(f"ending on {str(endtime).split('T')[0]}")
 
     start_user = params.get('start_user')
+    logger.info(f'start user: {start_user}')
     first_name = User.objects.get(username=start_user).first_name
     if first_name:
         summary.append(f"requested by {first_name}")
