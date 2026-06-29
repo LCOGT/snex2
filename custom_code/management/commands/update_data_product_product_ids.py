dbd96f385489ab8fd598f5c3941b64d360266606
Moira Andrews <moira.lee.andrews@gmail.com>
2026-06-29 16:40:57 -0700

    updated logging

--- custom_code/management/commands/update_data_product_product_ids.py
+++ custom_code/management/commands/update_data_product_product_ids.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from custom_code.scripts.sync_databases import get_spec_row_from_filename
 from tom_dataproducts.models import DataProduct
+from django.db import IntegrityError
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -14,24 +16,28 @@ class Command(BaseCommand):
 
         batch = []
         BATCH_SIZE = 500
-
+        logger.info(f'Total DataProducts to convert: {dps.count()}')
+        total = dps.count()
         for dp in dps.iterator(chunk_size=BATCH_SIZE):
             filename = dp.data.name.split('/')[-1].replace('.ascii','.fits')
             spec_row = get_spec_row_from_filename(filename)
-            if spec_row:
-                if spec_row.original:
-                    bname = spec_row.original.split('.')[0]
-                    logger.info(f'dp name: {dp.data.name} bname: {bname}')
-                    spec_filepath = "/".join(spec_row.filepath.split('/')[3:]) + spec_row.filename.replace('ascii', 'fits')
-                    dp.product_id = bname
-                    dp.data.name = spec_filepath
-                    batch.append(dp)
-                else:
-                    logger.info(f'No basename in spec table: {spec_row.id}')
+            try:
+                if spec_row:
+                    if spec_row.original:
+                        bname = spec_row.original.split('.')[0]
+                        spec_filepath = "/".join(spec_row.filepath.split('/')[3:]) + spec_row.filename.replace('ascii', 'fits')
+                        dp.product_id = bname
+                        dp.data.name = spec_filepath
+                        batch.append(dp)
+                    else:
+                        logger.info(f'No basename in spec table: {spec_row.id}')
+            except IntegrityError:
+                logger.error(f'Duplicate DataProduct with basename: {bname} {dp.data.name}')
             if len(batch) >= BATCH_SIZE:
                 DataProduct.objects.bulk_update(batch, ['product_id', 'data'])
                 batch.clear()
-                logger.info(f'Batch updated {len(batch)}')
+                total -= batch
+                logger.info(f'Batch updated, {total} remaining')
                 
 
         if batch:
