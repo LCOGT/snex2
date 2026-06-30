e2aa6b62719d0117c703c62dc500ed8256abf0aa
Moira Andrews <moira.lee.andrews@gmail.com>
2026-06-29 17:53:04 -0700

    fixed for real this time

--- custom_code/management/commands/update_data_product_product_ids.py
+++ custom_code/management/commands/update_data_product_product_ids.py
@@ -11,27 +11,24 @@ class Command(BaseCommand):
     help = 'Update the DataProduct table to have basenames as the product_id'
 
     def handle(self, *args, **kwargs):
-        dps = DataProduct.objects.only('id', 'product_id', 'data').filter(data_product_type='spectroscopy')
+        dps = DataProduct.objects.filter(data_product_type='spectroscopy')
 
         batch = []
         BATCH_SIZE = 500
         logger.info(f'Total DataProducts to convert: {dps.count()}')
         total = dps.count()
         for dp in dps.iterator(chunk_size=BATCH_SIZE):
-            try:
-                filename = dp.data.name.split('/')[-1].replace('.ascii','.fits')
-                spec_row = get_spec_row_from_filename(filename)
-                if spec_row:
-                    if spec_row.original:
-                        bname = spec_row.original.split('.')[0]
-                    else:
-                        bname = spec_row.filename
-                    spec_filepath = "/".join(spec_row.filepath.split('/')[3:]) + spec_row.filename.replace('ascii', 'fits')
-                    dp.product_id = bname
-                    dp.data.name = spec_filepath
-                    batch.append(dp)
-            except Exception as e:
-                logger.error(f'Skipping dp {dp.id} due to {e}')
+            filename = dp.data.name.split('/')[-1].replace('.ascii','.fits')
+            spec_row = get_spec_row_from_filename(filename)
+            if spec_row:
+                if spec_row.original:
+                    bname = spec_row.original.split('.')[0]
+                else:
+                    bname = spec_row.filename
+                spec_filepath = "/".join(spec_row.filepath.split('/')[3:]) + spec_row.filename.replace('ascii', 'fits')
+                dp.product_id = bname
+                dp.data.name = spec_filepath
+                batch.append(dp)
             if len(batch) >= BATCH_SIZE:
                 total -= len(batch)
                 DataProduct.objects.bulk_update(batch, ['product_id', 'data'])
